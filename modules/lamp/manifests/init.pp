4e466e95367343ebb6497bff338dc674025f07e9
dziuba <dziuba@dziuba-puppet.example.com>
2015-07-06 14:40:34 +0100

    lamp

--- modules/lamp/manifests/init.pp
+++ modules/lamp/manifests/init.pp
@@ -1,11 +1,17 @@
 class lamp {
-	package { [ 'apache2', 'mysql-server', 'php5-mysql']:
+	package { 
+		['apache2',
+		'mysql-server',
+		'php5-mysql'
+		'php5', 
+		'libapache2-mod-php5',
+		'php5-mcrypt' ]:
 		ensure => installed,
 	}
 	
 	service { 'apache2':
-	 	ensure => running,
-		enable => true,
+	 	ensure  => running,
+		enable  => true,
 		require => Package['apache2'],
 	}
 }
